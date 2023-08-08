55b605c8418a360ff3b4aa7628a6289433638f02
Lyuei <leeyuwei02@outlook.com>
2023-08-09 00:40:44 +0800

    remove csv files for users, only linked list

--- CSVReader.hpp
+++ CSVReader.hpp
@@ -3,9 +3,5 @@
 #include "LinkedList.hpp"
 #include "Property.hpp"
 
-void readAdmins(LinkedList& list);
-void readManagers(LinkedList& list);
-void readTenants(LinkedList& list);
-void deleteTenantFromFile(const string& filename, const string& userIdToDelete);
 void readProperties(PropertyLinkedList &propertyList);
 
