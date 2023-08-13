f75f2eade1e9ac4d030965270693aadf0eea5efe
Lyuei <leeyuwei02@outlook.com>
2023-08-13 16:38:46 +0800

    1

--- PaymentManager.cpp
+++ PaymentManager.cpp
@@ -2,22 +2,48 @@
 #include <iostream>
 
 bool PaymentManager::manageTenancyPayment(RentRequest &rentRequest) {
+    // Check if the rent request is approved
+    if (rentRequest.status != RentRequestStatus::APPROVED) {
+        std::cout << "Rent request is not approved. Payment cannot be processed.\n";
+        return false;
+    }
+
+    // Calculate the total amount (this might include rent, deposit, etc.)
+    int monthlyRent;
+
+    // Process the payment (this could involve integration with a payment gateway)
+    if (!processPayment(monthlyRent)) {
+        std::cout << "Payment failed. Please try again.\n";
+        return false;
+    }
+
+    // Update the rent request status to paid
+    rentRequest.status = RentRequestStatus::PAID;
+
+    // Record the payment in the tenant's payment history
+    recordPayment(rentRequest);
+
+    // Generate and provide a receipt
+    generateReceipt(rentRequest);
+
+    std::cout << "Payment successful. Thank you for your payment.\n";
+    return true;
+}
+
+
+
+
+bool PaymentManager::processPayment(double amount)
+{
     // Implementation...
 }
 
-double PaymentManager::calculateTotalAmount(const RentRequest &rentRequest) {
+void PaymentManager::recordPayment(const RentRequest &rentRequest)
+{
     // Implementation...
 }
 
-bool PaymentManager::processPayment(double amount) {
+void PaymentManager::generateReceipt(const RentRequest &rentRequest)
+{
     // Implementation...
 }
-
-void PaymentManager::recordPayment(const RentRequest &rentRequest) {
-    // Implementation...
-}
-
-void PaymentManager::generateReceipt(const RentRequest &rentRequest) {
-    // Implementation...
-}
-
